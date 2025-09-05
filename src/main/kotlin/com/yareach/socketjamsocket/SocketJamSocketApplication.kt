7273fb882f1f3f54abd229050a2ecbaa5d71cde0
yareach8345 <mine8345@naver.com>
2025-09-05 16:48:23 +0900

    init - 프로젝트 생성

---
+++ src/main/kotlin/com/yareach/socketjamsocket/SocketJamSocketApplication.kt
@@ -0,0 +1,11 @@
+package com.yareach.socketjamsocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocketJamSocketApplication
+
+fun main(args: Array<String>) {
+	runApplication<SocketJamSocketApplication>(*args)
+}
---
+++ src/test/kotlin/com/yareach/socketjamsocket/SocketJamSocketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yareach.socketjamsocket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocketJamSocketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
